b2dc240128993cc9ab7b1dc4b5675eb68787c38a
Cláudio <claudioalvesoficial@gmail.com>
2025-06-16 04:55:58 -0300

    Remove o arquivo role.schema.ts e atualiza os campos createdAt e updatedAt para created_at e updated_at no UserSchema, padronizando a nomenclatura dos campos para snake_case.

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -4,8 +4,8 @@ export const UserSchema = z.object({
   id: z.string().uuid(),
   name: z.string(),
   email: z.string().email(),
-  createdAt: z.string().or(z.date()),
-  updatedAt: z.string().or(z.date()),
+  created_at: z.string().or(z.date()),
+  updated_at: z.string().or(z.date()),
 });
 
 export type UserType = z.infer<typeof UserSchema>;
